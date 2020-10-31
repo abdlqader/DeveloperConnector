9bc416e33fbfafc3491077d20fea7a78698e3167
abdulkader <abdulkader_hassan74@hotmail.com>
2020-10-31 09:48:26 +0200

    setup the production scripts

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -21,7 +21,7 @@ const Post = ({ getPostById, post: { post, loading }, match }) => {
       </Link>
       <PostsItems post={post} showButtons={false} />
       <CommentForm postId={post._id} />
-      <div class="comments">
+      <div className="comments">
         {post.comments.map((comment) => (
           <CommentItem key={comment._id} comment={comment} postId={post._id} />
         ))}
